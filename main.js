465e6acb2572ac206f6b15764d53e3570641b047
Patrick <71256773+patrick-blip@users.noreply.github.com>
2022-11-01 00:36:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
 // Constants
-const PORT = 8080;
-const HOST = "0.0.0.0";
+// const PORT = 8080;
+// const HOST = "0.0.0.0";
 
 // App
 const app = express();
@@ -10,6 +10,6 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(3000, () => {
+  console.log(`Running on http://3000`);
 });
